3687245b7e6adb9a100102357d823af3693a65af
maksim <maksymchybisov@gmail.com>
2018-09-07 15:59:07 +0300

    task 2

--- index.php
+++ index.php
@@ -1,45 +1,54 @@
 <?php
 
-
-     function arraySum (array $arr){
+class toArray
+{
+    public function arraySum(array $arr)
+    {
         $sum = 0;
-        foreach ($arr as $item){
-            if (is_int($item)){
+        foreach ($arr as $item) {
+            if (is_int($item)) {
                 $sum += $item;
             }
         }
-         return $sum;
+        return $sum;
     }
 
-     function inArray ($needly, array $arr, $strict = FALSE){
-        foreach ($arr as $item){
-            if ($strict == FALSE && $needly == $item){
+    public function inArray($needly, array $arr, $strict = FALSE)
+    {
+        foreach ($arr as $item) {
+            if ($strict == FALSE && $needly == $item) {
                 return TRUE;
-            }elseif ($strict == TRUE && $needly === $item){
+            } elseif ($strict == TRUE && $needly === $item) {
                 return TRUE;
-            }else{
+            } else {
                 continue;
             }
         }
         return FALSE;
     }
 
-     function arrayDiff (array $arr1, array $arr2){
-        foreach ($arr1 as $key => $value){
-            if (in_array($value, $arr2)){
+    public function arrayDiff(array $arr1, array $arr2)
+    {
+        foreach ($arr1 as $key => $value) {
+            if (in_array($value, $arr2)) {
+
                 unset($arr1[$key]);
             }
         }
         return $arr1;
-
     }
 
-$arr = [];
-$arr1 = [1,2,3,4,5,6,7,8,9];
-$arr2 = [12,24,36,48,"sfsdfs",5,6,7,8,9];
-
-
-
-var_dump(arrayDiff($arr2, $arr1));
-
-//in_array
+    public function sortN(array $arr)
+    {
+        for ($i = 0; $i < count($arr); $i++) {
+            for ($j = $i + 1; $j < count($arr); $j++) {
+                if ($arr[$i] > $arr[$j]) {
+                    $temp = $arr[$j];
+                    $arr[$j] = $arr[$i];
+                    $arr[$i] = $temp;
+                }
+            }
+        }
+        return $arr;
+    }
+}
